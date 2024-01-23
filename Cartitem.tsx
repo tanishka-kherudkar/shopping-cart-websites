5328460051ea38cb4de77120ad2c5c3f7b61276d
tanishka-kherudkar <124390249+tanishka-kherudkar@users.noreply.github.com>
2024-01-23 13:31:46 +0530

    Add files via upload

---
+++ Cartitem.tsx
@@ -0,0 +1,44 @@
+import Button from '@material-ui/core/Button';
+// Types
+import { CartitemType } from '../App';
+// Styles
+import { Wrapper } from './Cartitem.styles';
+
+type Props = {
+  item: CartitemType;
+  addToCart: (clickedItem: CartitemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const Cartitem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+  <Wrapper>
+    <div>
+      <h3>{item.title}</h3>
+      <div className='information'>
+        <p>Price: ${item.price}</p>
+        <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+      </div>
+      <div className='buttons'>
+        <Button
+          size='small'
+          disableElevation
+          variant='contained'
+          onClick={() => removeFromCart(item.id)}
+        >
+          -
+        </Button>
+        <p>{item.amount}</p>
+        <Button
+          size='small'
+          disableElevation
+          variant='contained'
+          onClick={() => addToCart(item)}
+        >
+          +
+        </Button>
+      </div>
+    </div>
+    <img src={item.image} alt={item.title} />
+  </Wrapper>
+);
+
